b8b49454de208c8bea7b194932fae93258546ab1
Sarah Moran <s.l.a.moran@gmail.com>
2023-05-15 18:15:41 +0100

    updated filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,30 +3,29 @@ import Axios from "axios";
 
 const Filter = () => {
   const [currencies, setCurrencies] = useState([]);
-  const [id, setID] = useState([]);
+  const [eventValue, setEventValue] = useState();
 
   const getFilteredTrades = async () => {
-    const response = await Axios.get(
-      `http://localhost:3000/tradeHistory/${id}`
-    );
+    const response = await Axios.get(`http://localhost:3000/tradeHistory/`);
     setCurrencies(response.data);
     console.log(response);
+    console.log(response.data);
   };
 
   useEffect(() => {
     getFilteredTrades();
   }, []);
 
-  console.log(id);
+  console.log(eventValue);
 
   return (
     <div>
-      <select onChange={(e) => setID(e.target.value)}>
+      <select onChange={(e) => setEventValue(e.target.value)}>
         <option></option>
         {currencies
           ? currencies.map((trade) => {
               return (
-                <option key={trade.id} value={trade.id}>
+                <option key={trade.id} value={trade.currency_crypto}>
                   {trade.currency_crypto}
                 </option>
               );
