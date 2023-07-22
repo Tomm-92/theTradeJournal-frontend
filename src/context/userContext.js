8d17d4d3386c3d24f80bad3f08c154d320eaf052
Tom Wilkinson <thomasmarkwilkinson@gmail.com>
2023-07-22 15:24:45 +0100

    user context route updated to render

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -34,19 +34,15 @@ export const UserContextProvider = ({ children }) => {
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         axios
-          .post(
-            "http://localhost:3000/users/",
-            //"https://thetradejournal-backend.onrender.com/users/",
-            {
-              headers: {
-                "x-api-key": process.env.REACT_APP_USERS_API_KEY,
-              },
-              email_address: `${userCredential.user.email}`,
-              firebase_uid: `${userCredential.user.uid}`,
-              first_name: first_name,
-              last_name: last_name,
-            }
-          )
+          .post("https://thetradejournal-backend.onrender.com/users/", {
+            headers: {
+              "x-api-key": process.env.REACT_APP_USERS_API_KEY,
+            },
+            email_address: `${userCredential.user.email}`,
+            firebase_uid: `${userCredential.user.uid}`,
+            first_name: first_name,
+            last_name: last_name,
+          })
           .catch((error) => {
             console.log(error);
           });
